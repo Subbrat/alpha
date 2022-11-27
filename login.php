025b8a6cca21a9cbde50bdabfb9262d43d4de301
Subrat Mallick <m.subrat198470@gmail.com>
2022-11-27 20:55:03 +0530

    asad

--- login.php
+++ login.php
@@ -3,31 +3,31 @@
 <html>
 
 <head>
-	<title>Registration system PHP and MySQL</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <title>Alpha - Login</title>
+    <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+    <link rel="stylesheet" type="text/css" href="./s6css.css">
+    <link rel="stylesheet" type="text/css" href="./main.css">
 </head>
 
-<body>
-	<div class="header">
-		<h2>Login</h2>
-	</div>
-	<form method="post" action="login.php">
-		<?php include('errors.php'); ?>
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username">
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password">
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_user">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
+<body class="s6-center s6-margin-top s6-displaycontainer">
+    <div class=" s6-cyan s6-center s6-display-middle main s6-round-large">
+        <p class="s6-xlarge">Login </p>
+        <form method="post" action="login.php">
+            <?php include('errors.php'); ?>
+            <div>
+                <input type="text" class="input" name="username" autocomplete="off" placeholder="username">
+            </div>
+            <div>
+                <input type="password" class="input" name="password" autocomplete="off" placeholder="password">
+            </div><br>
+            <div>
+                <button type="submit" class="btn" name="login_user">Login</button>
+            </div>
+            <p>
+                Not yet a member? <a href="register.php" class="s6-hover-text-green">Sign up</a>
+            </p>
+        </form>
+    </div>
 </body>
 
 </html>
--- server.php
+++ server.php
@@ -56,10 +56,10 @@ if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
   if (empty($username)) {
-    array_push($errors, "Username is required");
+    ;
   }
   if (empty($password)) {
-    array_push($errors, "Password is required");
+    ;
   }
   if (count($errors) == 0) {
     $password = md5($password);
